fc7862aa302b30b722f11f09bcaf7206000b05b3
Afke Holgersen <Afke@d207-023-176-087.wireless.sfu.ca>
2016-09-28 11:54:58 -0700

    commit to git:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function addRow(tableID){
+  var table= document.getElementByID(tableID);
+  var rowCount=table.rows.length;
+
+
+  var row=table.insertRow(rowCount);
+
+  var name=row.insertCell(0);
+  var nametext= "Activity ".concat(rowCount);
+
+  var short=row.insertCell(1);
+  var shorttext= "A ".concat(rowCount);
+
+
+  var weight=row.insertCell(2);
+  var grade=row.insertCell(3);
+  var percent=row.insertCell(4);
+}
+function
